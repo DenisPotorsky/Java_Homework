154de15f8164a475a7fa1eb04d7d26d74b750a3d
Den <denis-s2@yandex.ru>
2023-01-23 12:06:23 +0500

    поправил домашку

--- Sem1/Calculator.java
+++ Sem1/Calculator.java
@@ -2,24 +2,38 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
-        System.out.print("Введите число a: ");
-        int a = new Scanner(System.in).nextInt();
-        System.out.print("Введите число b: ");
-        int b = new Scanner(System.in).nextInt();
-        System.out.print("Введите операцию + - * / : ");
-        String op = new Scanner(System.in).nextLine();
-
-        if (op.equals("+")) {
-            System.out.println(a + b);
-        }
-        if (op.equals("-")) {
-            System.out.println(a - b);
-        }
-        if (op.equals("*")) {
-            System.out.println(a * b);
-        }
-        if (op.equals("/")) {
-            System.out.println(a / b);
+        System.out.println("Чтобы выйти из калькулятора - введите команду Stop"); 
+        Scanner sc = new Scanner(System.in);
+        int a = 0;
+        int b = 0;
+        String op = "";
+        while (true) {
+            System.out.println("Введите число a: ");
+  
+            if(sc.hasNextInt()) a = sc.nextInt(); 
+            else System.out.println("Вы ввели не число");
+            
+            
+            System.out.print("Введите число b: ");
+            if(sc.hasNextInt()) b = sc.nextInt(); 
+            else System.out.println("Вы ввели не число");
+            
+            System.out.print("Введите операцию + - * / : ");
+            sc.nextLine();
+            op = sc.nextLine();
+            if (op.equals("Stop")) break;
+            if (op.equals("+")) {
+                System.out.println(a + b);
+            }
+            if (op.equals("-")) {
+                System.out.println(a - b);
+            }
+            if (op.equals("*")) {
+                System.out.println(a * b);
+            }
+            if (op.equals("/")) {
+                System.out.println(a / b);
+            }
         }
     }
 }
